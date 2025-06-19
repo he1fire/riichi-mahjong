9129c1b8507eeaacedbccc2bd040b35c45d80782
he1fire <cmg3172@naver.com>
2025-06-19 14:34:12 +0900

    Add typescript branch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import i18n from "./i18n/i18n"
+import i18n from './i18n/i18n.ts'
 
 const app = createApp(App)
 app.use(i18n)
