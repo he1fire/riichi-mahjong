b601a80a04b5d6f30bfbdd9b39b718c0fefbfd41
he1fire <cmg3172@naver.com>
2025-07-03 11:04:20 +0900

    Add router for i18n

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import i18n from './i18n/i18n.ts'
+import '@/style.css'
+import App from '@/App.vue'
+import router from '@/router/index.ts'
+import i18n from '@/i18n/i18n.ts'
 
 const app = createApp(App)
+app.use(router)
 app.use(i18n)
 app.mount('#app')
