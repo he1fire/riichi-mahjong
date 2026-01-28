7b0fab54b3206fa5eb5941f26eae7786442604a3
he1fire <cmg3172@naver.com>
2026-01-28 09:34:42 +0900

    Edit dynamicRoute for sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,16 @@ export default defineConfig({
   plugins: [
     vue(),
     sitemap({
-      hostname: 'https://he1fire.github.io/riichi-mahjong/',
-      dynamicRoutes: ['/ko', '/en', '/ja'],
+      // 1. hostname은 도메인까지만 적어줍니다.
+      hostname: 'https://he1fire.github.io', 
+      // 2. 경로에 프로젝트 이름을 직접 포함시킵니다.
+      dynamicRoutes: [
+        '/riichi-mahjong/ko',
+        '/riichi-mahjong/en',
+        '/riichi-mahjong/ja'
+      ],
+      // 3. 필요 없는 404 페이지는 제외합니다.
+      exclude: ['/404'],
       outDir: 'dist'
     })
   ],
