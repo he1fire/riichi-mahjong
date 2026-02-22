a21d347094d9d34446623c5b44120fe19e72f4a5
he1fire <cmg3172@naver.com>
2026-02-23 00:07:44 +0900

    Fix: remove unnecessary props in ModalRecordList and ModalRollback

--- src/components/modals/stats/ModalRecordList.vue
+++ src/components/modals/stats/ModalRecordList.vue
@@ -10,7 +10,7 @@ interface Props {
   players: Player[],
   records: Records
 }
-const props = defineProps<Props>()
+defineProps<Props>()
 
 /**emits 정의*/
 type Emits = {
--- src/components/modals/stats/ModalRollback.vue
+++ src/components/modals/stats/ModalRollback.vue
@@ -10,7 +10,7 @@ interface Props {
   records: Records,
   modalInfo: ModalInfo
 }
-const props = defineProps<Props>()
+defineProps<Props>()
 
 /**emits 정의*/
 type Emits = {
